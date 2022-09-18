6b4f37ba950bf3145948a18d79a766813dc72fd4
dobriy-yashcher <golovinsergey17@gmail.com>
2022-09-18 21:58:10 +0300

    Add Sword.

---
+++ WeaponCore/Weapons/Sword.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeaponCore.Interfaces;
+
+namespace WeaponCore.Weapons
+{
+    public class Sword : IMleeWeapon
+    {
+        public int MleeDamage { get; set; }
+        public int Condition { get; set; }
+
+        public Sword(int mleeDamage, int condition)
+        {
+            MleeDamage = mleeDamage;
+            Condition = condition;
+        }
+
+        public void Hit()
+        {
+            if (Condition <= 0)
+            {
+                Console.WriteLine("Sword is broken");
+            }
+
+            else
+            {
+                --Condition;
+                Console.WriteLine($"Sword hit with {MleeDamage}.");
+            }
+        }
+
+        public void Repair(int countConditionRepaired)
+        {
+            Condition += countConditionRepaired;
+
+            Console.WriteLine($"Sword has repaired by {countConditionRepaired} points.");
+        }
+
+        public void UpgradeDamage(int countDamageUpgrade)
+        {
+            MleeDamage += countDamageUpgrade;
+
+            Console.WriteLine($"Sword damage has upgrade by {countDamageUpgrade} points.");
+        }
+    }
+}
