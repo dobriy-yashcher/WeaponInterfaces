f386ef8e47cc34c2b991e842092999ec035dc27e
dobriy-yashcher <golovinsergey17@gmail.com>
2022-09-18 13:24:35 +0300

    Add project files.

---
+++ WeaponCore/IReload.cs
@@ -0,0 +1,6 @@
+﻿namespace WeaponCore
+{
+    public interface IReload
+    {
+    }
+}
---
+++ WeaponCore/IWeapon.cs
@@ -0,0 +1,11 @@
+﻿namespace WeaponCore
+{
+    public interface IWeapon : IReload, IRepairible, IUpgradeble
+    {
+        int Damage { get; set; }
+
+        void SingleShoot();
+
+        void ShowInfo();
+    }
+}
